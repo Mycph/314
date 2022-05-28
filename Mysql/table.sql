414a280e0e6c5ce5bb1dc0715fe68ce3fa340a38
zeming666 <zr109@uowmail.edu.au>
2022-05-28 20:14:29 +1000

    sql

--- Mysql/table.sql
+++ Mysql/table.sql
@@ -28,19 +28,12 @@ CREATE TABLE Professional
     gender   VARCHAR(10) not null,
     primary key (proNum)
 );
-CREATE TABLE Vehicle
-(
-    plateNum varchar(50) PRIMARY KEY,
-    model    VARCHAR(150) NOT NULL,
-    cusNum   INT          NOT NULL,
-    FOREIGN KEY (cusNum) references Customer (cusNum)
-);
+
 CREATE TABLE orders
 (
     orderid        int,
     orderStartDate DATE,
     vehiclePlate varchar(50),
-    price          FLOAT,
     location       VARCHAR(80),
     issue          varchar(150),
     O_cusNum         int not null,
@@ -50,6 +43,8 @@ CREATE TABLE orders
     rating         FLOAT,
     payCardNum     varchar(50),
     payType        varchar(20),
+    EstimatePayment     float,
+    actualPayment       float,
     primary key orders (orderid),
     foreign key (O_cusNum) references Customer (cusNum),
     foreign key (O_proNum) references Professional (proNum)
@@ -59,12 +54,13 @@ CREATE TABLE cur_orders
     cur_orderid        int auto_increment,
     orderStartDate DATE,
     vehiclePlate varchar(50),
-    price          FLOAT,
+    EstimatePayment          FLOAT,
     c_location       VARCHAR(80),
     issue          varchar(150),
     O_cusNum         int not null,
     O_proNum         int,
     sstate          varchar(30),
+    isVip           varchar(20),
     primary key cur_orders (cur_orderid),
     foreign key (O_cusNum) references Customer (cusNum)
 );
@@ -89,6 +85,9 @@ create trigger delCurrOrder after insert
     begin
         delete from cur_orders where cur_orderid = New.orderid;
     end;
+
+
+
 drop trigger updateBal;
 
 drop table orders;
@@ -97,26 +96,3 @@ drop table customer;
 drop table cur_orders;
 
 drop table Professional;
-
-
--- # insert into customer(cusName, phoneNum, vipStart, vipEnd, email, gender)
--- #     value ('name', 'phoneNum', '1999-10-12', '2000-10-12', 'email', 'male');
--- #
--- # insert into professional(proName, phoneNum,pLevel,balance,location, email, gender)
--- #     value ('pname2', 'phoneNum',10,1100,'location','email', 'male');
--- #
--- # insert into orders(orderStartDate,cusNum,price,vehiclePlate,location,issue,proNum,orderEndDate,review,rating,payCardNum,payType)
--- # value ('1999-10-12',1,50,'plate','loaction','issue',3,'1999-10-12','124321535145124123review',
--- #       3,'12415135','income');
--- #
--- # insert into CUSTOMER (cusPw,cusName,cusDOB,phoneNum,vipStart,vipEnd,email,gender) values
--- # ('passw', 'name', '1999-10-12', '14141', '1999-10-12', '1999-10-12','111@131','ma');
--- #
--- # insert into Vehicle(plateNum, model, cusNum) VALUE ('plateNum','BMW',1);
--- # commit ;
--- #
--- #
--- # UPDATE customer  SET cusName='123',gender='male',cusDOB='2022-05-19',phoneNum='1231',cusPw='123133',email='123@312.com',plateNum='1231',vehicleModel='123123' WHERE cusNum = 8;
--- #
--- #
-
